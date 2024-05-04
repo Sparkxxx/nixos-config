7e7ceb9cccdc27e0394d937aa5dd508cc5af5426
sparkx <dbraileanu@gmail.com>
2024-05-04 16:57:46 +0300

    more

--- modules/nixos/base/networking.nix
+++ modules/nixos/base/networking.nix
@@ -3,13 +3,13 @@ _: {
   # With this enabled, you can access your machine at <hostname>.local
   # it's more convenient than using the IP address.
   # https://avahi.org/
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    publish = {
-      enable = true;
-      domain = true;
-      userServices = true;
-    };
-  };
+  # services.avahi = {
+  #   enable = true;
+  #   nssmdns4 = true;
+  #   publish = {
+  #     enable = true;
+  #     domain = true;
+  #     userServices = true;
+  #   };
+  # };
 }

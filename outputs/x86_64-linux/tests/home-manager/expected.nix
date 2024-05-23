d7a605dfac8b7397d89a0994232e4c3d16ac0479
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-05-23 09:01:21 +0300

    more

--- outputs/x86_64-linux/tests/home-manager/expected.nix
+++ outputs/x86_64-linux/tests/home-manager/expected.nix
@@ -4,12 +4,12 @@
 }: let
   username = myvars.username;
   hosts = [
-    "ai-hyprland"
-    "ai-i3"
-    "shoukei-hyprland"
-    "shoukei-i3"
-    "ruby"
-    "k3s-prod-1-master-1"
+    # "ai-hyprland"
+    # "ai-i3"
+    # "shoukei-hyprland"
+    # "shoukei-i3"
+    # "ruby"
+    # "k3s-prod-1-master-1"
     "twr-z790-hyprland"
     "twr-z790-i3"
   ];

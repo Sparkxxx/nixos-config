212465221ae38aa79507ecd19712196469811da8
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-05-31 13:52:35 +0300

    more

--- home/linux/gui/plasma/values/plasma.nix
+++ home/linux/gui/plasma/values/plasma.nix
@@ -1,16 +1,26 @@
 {
   pkgs,
   lib,
-  #plasma,
+  plasma,
   #nur-ryan4yin,
   ...
 }: let
   package = plasma.packages.${pkgs.system}.plasma;
 in {
 
-wayland.windowManager.plasma = {
-    inherit package;
-    enable = true;
-    systemd.enable = true;
-  };
+services.xserver = {
+		# Required by SDDM.
+		enable = true;
+		displayManager.sddm = {
+			enable = true;
+			wayland.enable = true;
+		};
+		# Enable Desktop Environment.
+		desktopManager.plasma5.enable = true;
+		# Configure keymap in X11.
+		layout = user.services.xserver.layout;
+		xkbVariant = user.services.xserver.xkbVariant;
+		# Exclude default X11 packages I don't want.
+		excludePackages = with pkgs; [ xterm ];
+	};
 }

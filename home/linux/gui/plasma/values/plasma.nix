46154349d6f387a85663813a32065b29eb033257
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-05-31 13:16:15 +0300

    more

--- home/linux/gui/plasma/values/plasma.nix
+++ home/linux/gui/plasma/values/plasma.nix
@@ -1,6 +1,44 @@
 {
+  pkgs,
   lib,
+  plasma,
+  #nur-ryan4yin,
   ...
-}: {
+}: let
+  package = plasma.packages.${pkgs.system}.plasma;
+in {
+  # NOTE:
+  # We have to enable hyprland/i3's systemd user service in home-manager,
+  # so that gammastep/wallpaper-switcher's user service can be start correctly!
+  # they are all depending on hyprland/i3's user graphical-session
+  wayland.windowManager.plasma = {
+    inherit package;
+    enable = true;
+    # settings = {
+    #   source = "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-hyprland}/themes/mocha.conf";
+    #   env = [
+    #     "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
+    #     "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
+    #     "MOZ_WEBRENDER,1"
+    #     # misc
+    #     "_JAVA_AWT_WM_NONREPARENTING,1"
+    #     "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+    #     "QT_QPA_PLATFORM,wayland"
+    #     "SDL_VIDEODRIVER,wayland"
+    #     "GDK_BACKEND,wayland"
+    #   ];
+    # };
+    # extraConfig = builtins.readFile ../conf/hyprland.conf;
+    # # gammastep/wallpaper-switcher need this to be enabled.
+    # systemd.enable = true;
+  };
+
+  # NOTE: this executable is used by greetd to start a wayland session when system boot up
+  # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
+  # home.file.".wayland-session" = {
+  #   source = "${package}/bin/Hyprland";
+  #   executable = true;
+  # };
+
   
 }

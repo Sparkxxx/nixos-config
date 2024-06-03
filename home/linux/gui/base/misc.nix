f3fee3fe44b525cb75174b86e355ac2f6c57b969
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-06-03 13:54:12 +0300

    more

--- home/linux/gui/base/misc.nix
+++ home/linux/gui/base/misc.nix
@@ -5,8 +5,11 @@
 }: {
   home.packages = with pkgs; [
     # GUI apps
+    
+    # Browsers
     firefox
- 
+    brave
+    ungoogled-chromium
  
     # e-book viewer(.epub/.mobi/...)
     # do not support .pdf
@@ -19,13 +22,16 @@
     whatsapp-for-linux
     #pkgs-unstable.qq # https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/networking/instant-messengers/qq
 
+    # Email 
+    betterbird
+
     # remote desktop(rdp connect)
     remmina
     freerdp # required by remmina
 
     # misc
     flameshot
-    ventoy # multi-boot usb creator
+    ventoy # cli multi-boot usb creator
 
   ];
 

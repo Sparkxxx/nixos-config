910f7f67aed33fc41755db9722d3c341ac202832
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-06-03 09:28:12 +0300

    more

--- home/linux/gui/plasma/options/nvidia.nix
+++ home/linux/gui/plasma/options/nvidia.nix
@@ -11,14 +11,14 @@ in {
   };
 
   config = mkIf (cfg.enable && cfg.nvidia) {
-    wayland.windowManager.plasma.settings.env = [
-      # for plasma with nvidia gpu, ref https://wiki.hyprland.org/Nvidia/
-      "LIBVA_DRIVER_NAME,nvidia"
-      "XDG_SESSION_TYPE,wayland"
-      "GBM_BACKEND,nvidia-drm"
-      "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-      # fix https://github.com/hyprwm/Hyprland/issues/1520
-      "WLR_NO_HARDWARE_CURSORS,1"
-    ];
+    # wayland.windowManager.plasma.settings.env = [
+    #   # for plasma with nvidia gpu, ref https://wiki.hyprland.org/Nvidia/
+    #   "LIBVA_DRIVER_NAME,nvidia"
+    #   "XDG_SESSION_TYPE,wayland"
+    #   "GBM_BACKEND,nvidia-drm"
+    #   "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+    #   # fix https://github.com/hyprwm/Hyprland/issues/1520
+    #   "WLR_NO_HARDWARE_CURSORS,1"
+    # ];
   };
 }

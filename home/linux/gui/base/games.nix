57f5f02f14327ab88e465bcad03b9d30902df874
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-06-04 13:05:00 +0300

    more

--- home/linux/gui/base/games.nix
+++ home/linux/gui/base/games.nix
@@ -8,5 +8,6 @@
     gamescope # SteamOS session compositing window manager
     prismlauncher # A free, open source launcher for Minecraft
     winetricks # A script to install DLLs needed to work around problems in Wine
+    grapejuice # Simple Wine+Roblox management tool
   ];
 }
--- home/linux/gui/plasma/values/packages.nix
+++ home/linux/gui/plasma/values/packages.nix
@@ -27,5 +27,11 @@
     # mpc-cli # command-line mpd client
     # ncmpcpp # a mpd client with a UI
     # networkmanagerapplet # provide GUI app: nm-connection-editor
+    kdePackages.kdenlive # Video editor
+    kdePackages.zanshin # Getting Things Done application
+    kdePackages.yakuake # Drop-down terminal emulator based on Konsole technologies
+    kdePackages.umbrello # GUI for diagramming Unified Modelling Language (UML)
+    kdePackages.tokodon # Tokodon is a Mastodon client for Plasma and Plasma Mobile
+    kdePackages.kdeconnect-kde # Multi-platform app that allows your devices to communicate
   ];
 }

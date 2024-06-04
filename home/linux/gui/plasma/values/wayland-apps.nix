1c5ef0f21fa4617119f4e848056245c511db74c3
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-06-04 14:17:39 +0300

    more

--- home/linux/gui/plasma/values/wayland-apps.nix
+++ home/linux/gui/plasma/values/wayland-apps.nix
@@ -93,18 +93,5 @@
     #     # });
     # };
 
-    kdePackages.kdenlive # Video editor
-    kdePackages.glaxnimate # Simple vector animation program.- required by kdenlive
-    kdePackages.spectacle # Screen recorder
-    kdePackages.zanshin # Getting Things Done application
-    kdePackages.yakuake # Drop-down terminal emulator based on Konsole technologies
-    #kdePackages.umbrello # GUI for diagramming Unified Modelling Language (UML) - 24.05.0 is broken
-    kdePackages.tokodon # Tokodon is a Mastodon client for Plasma and Plasma Mobile
-    kdePackages.neochat # A client for matrix, the decentralized communication protocol
-    kdePackages.kdeconnect-kde # Multi-platform app that allows your devices to communicate
-    kdePackages.kmail # State-of-the-art feature-rich email client that supports many protocols
-    kdePackages.kmail-account-wizard # Application which assists you with the configuration of accounts in KMail
-    kdePackages.kdepim-addons # Add-ons for KDE PIM apps (KMail, KAddressBook etc.)
-    kdePackages.kmailtransport # Library, KCM and KDED module to manage mail transport
   };
 }

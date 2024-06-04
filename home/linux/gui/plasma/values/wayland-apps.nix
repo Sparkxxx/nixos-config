bcdc26bb75a71cef88c58bec572198d56b97742b
sparkx <15813839+Sparkxxx@users.noreply.github.com>
2024-06-04 13:52:17 +0300

    more

--- home/linux/gui/plasma/values/wayland-apps.nix
+++ home/linux/gui/plasma/values/wayland-apps.nix
@@ -36,23 +36,23 @@
     # };
 
     # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
-    google-chrome = {
-      enable = true;
+    # google-chrome = {
+    #   enable = true;
 
-      # https://wiki.archlinux.org/title/Chromium#Native_Wayland_support
-      commandLineArgs = [
-        "--ozone-platform-hint=auto"
-        "--ozone-platform=wayland"
-        # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
-        # (only supported by chromium/chrome at this time, not electron)
-        "--gtk-version=4"
-        # make it use text-input-v1, which works for kwin 5.27 and weston
-        "--enable-wayland-ime"
+    #   # https://wiki.archlinux.org/title/Chromium#Native_Wayland_support
+    #   commandLineArgs = [
+    #     "--ozone-platform-hint=auto"
+    #     "--ozone-platform=wayland"
+    #     # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+    #     # (only supported by chromium/chrome at this time, not electron)
+    #     "--gtk-version=4"
+    #     # make it use text-input-v1, which works for kwin 5.27 and weston
+    #     "--enable-wayland-ime"
 
-        # enable hardware acceleration - vulkan api
-        # "--enable-features=Vulkan"
-      ];
-    };
+    #     # enable hardware acceleration - vulkan api
+    #     # "--enable-features=Vulkan"
+    #   ];
+    # };
 
     firefox = {
       enable = true;
@@ -60,37 +60,37 @@
       package = pkgs.firefox-wayland; # firefox with wayland support
     };
 
-    vscode = {
-      enable = true;
-      # let vscode sync and update its configuration & extensions across devices, using github account.
-      userSettings = {};
-      package =
-        (pkgs.vscode.override
-          {
-            isInsiders = true;
-            # https://wiki.archlinux.org/title/Wayland#Electron
-            commandLineArgs = [
-              "--ozone-platform-hint=auto"
-              "--ozone-platform=wayland"
-              # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
-              # (only supported by chromium/chrome at this time, not electron)
-              "--gtk-version=4"
-              # make it use text-input-v1, which works for kwin 5.27 and weston
-              "--enable-wayland-ime"
+    # vscode = {
+    #   enable = true;
+    #   # let vscode sync and update its configuration & extensions across devices, using github account.
+    #   userSettings = {};
+    #   package =
+    #     (pkgs.vscode.override
+    #       {
+    #         isInsiders = true;
+    #         # https://wiki.archlinux.org/title/Wayland#Electron
+    #         commandLineArgs = [
+    #           "--ozone-platform-hint=auto"
+    #           "--ozone-platform=wayland"
+    #           # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+    #           # (only supported by chromium/chrome at this time, not electron)
+    #           "--gtk-version=4"
+    #           # make it use text-input-v1, which works for kwin 5.27 and weston
+    #           "--enable-wayland-ime"
 
-              # TODO: fix https://github.com/microsoft/vscode/issues/187436
-              # still not works...
-              "--password-store=gnome" # use gnome-keyring as password store
-            ];
-          });
-        # .overrideAttrs (oldAttrs: rec {
-        #   # Use VSCode Insiders to fix crash: https://github.com/NixOS/nixpkgs/issues/246509
-        #   src = builtins.fetchTarball {
-        #     url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-        #     sha256 = "1pp5hzcz326hww4qr6bl8p0ndp1fmjd0lnazc3j31sbkcbwr0v1w";
-        #   };
-        #   version = "latest";
-        # });
-    };
+    #           # TODO: fix https://github.com/microsoft/vscode/issues/187436
+    #           # still not works...
+    #           "--password-store=gnome" # use gnome-keyring as password store
+    #         ];
+    #       });
+    #     # .overrideAttrs (oldAttrs: rec {
+    #     #   # Use VSCode Insiders to fix crash: https://github.com/NixOS/nixpkgs/issues/246509
+    #     #   src = builtins.fetchTarball {
+    #     #     url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
+    #     #     sha256 = "1pp5hzcz326hww4qr6bl8p0ndp1fmjd0lnazc3j31sbkcbwr0v1w";
+    #     #   };
+    #     #   version = "latest";
+    #     # });
+    # };
   };
 }
